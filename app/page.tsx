e819e510f6717ba2c2d74f81cd51bb8b891967d3
idyWilliams <widorenyin0@gmail.com>
2024-08-11 17:13:34 +0100

    WIP: Login redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import DashTopNav from "@/components/lib/navigation/dash_topnav";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <Link href={"/signup"}> Goloka</Link>
-      <DashTopNav />
     </main>
   );
 }
