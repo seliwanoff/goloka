0cc97c4ba02dcc81c17cedce226a487de06d74b7
idyWilliams <widorenyin0@gmail.com>
2024-08-11 17:08:05 +0100

    WIP: Dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import DashTopNav from "@/components/lib/navigation/dash_topnav";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <Link href={"/signup"}> Goloka</Link>
+      <DashTopNav />
     </main>
   );
 }
