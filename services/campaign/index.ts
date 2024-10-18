563f4967cff43eb733ceac4d2a09109738b2130f
Williams <widorenyin0@gmail.com>
2024-10-18 13:24:01 +0100

    new updates and fixes

--- services/campaign/index.ts
+++ services/campaign/index.ts
@@ -13,6 +13,7 @@ import {
 
 import { UseQueryResult } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
+import { ServerResponseOrNull } from "../misc";
 
 // ~ =============================================>
 // ~ ======= Create a campaign response  -->
@@ -31,3 +32,37 @@ export const createCampaignResponse = async (
     },
   });
 };
+// ~ =============================================>
+// ~ ======= Create a campaign bookmark  -->
+// ~ =============================================>
+export const bookmarkCampaign = async (
+  data: any,
+  campaignId: string,
+): Promise<UseQueryResult<ServerResponse<any>>> => {
+  return queryClient.fetchQuery({
+    queryKey: ["bookmark"],
+    queryFn: async () => {
+      return await postData<ServerResponse<any>>(
+        `/contributor/campaigns/${campaignId}/bookmark/create`,
+        data,
+      );
+    },
+  });
+};
+// ~ =============================================>
+// ~ ======= delete a campaign bookmark  -->
+// ~ =============================================>
+export const removeBookmark = async (
+  taskId: string,
+): Promise<ServerResponseOrNull<any>> => {
+  try {
+    return await deleteDataById<ServerResponse<any>>(
+      `/contributor/campaigns/${taskId}/bookmark/delete`,
+    //   taskId,
+    );
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
