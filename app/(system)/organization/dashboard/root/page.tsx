b28ba7426cb813a2e15f4701dd16d8a22c160247
idyWilliams <widorenyin0@gmail.com>
2024-10-31 20:16:38 +0100

    Dashboard Charts

--- app/(system)/organization/dashboard/root/page.tsx
+++ app/(system)/organization/dashboard/root/page.tsx
@@ -3,26 +3,16 @@ import DashboardWidget from "@/components/lib/widgets/dashboard_card";
 import React, { useState } from "react";
 import {
   Calendar,
-  Calendar1,
-  ClipboardExport,
   DocumentUpload,
   Eye,
   Note,
   Setting4,
-  Task,
   TrendUp,
   Wallet3,
 } from "iconsax-react";
 import { Button } from "@/components/ui/button";
 import Pagination from "@/components/lib/navigation/Pagination";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -60,25 +50,8 @@ import {
   getStatusText,
   Status,
 } from "@/helper";
-import {
-  ChartConfig,
-  ChartContainer,
-  ChartLegend,
-  ChartLegendContent,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
-
-import { TrendingUp } from "lucide-react";
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Line,
-  LineChart,
-  XAxis,
-  YAxis,
-} from "recharts";
+import CampaignChart from "@/components/organization-comps/campaign_chart";
+import CampaignSummary from "@/components/organization-comps/campaign_summary";
 
 const Dashboard = () => {
   // const [filteredData, setFilteredData] = useState<Response[]>(
@@ -177,10 +150,46 @@ const Dashboard = () => {
 
       {/* CHART */}
       <div className="col-span-5 grid w-full grid-cols-[2fr_1fr] gap-6">
-        <div className="rounded-2xl bg-white p-[14px]">
-          <CampaignChart />
+        <div className="flex flex-col justify-evenly rounded-2xl bg-white p-6">
+          <div className="grid flex-1 gap-1 text-center sm:text-left">
+            <h3 className="text-base font-medium">Campaign against response</h3>
+          </div>
+
+          <div className="flex flex-col items-center gap-10">
+            <CampaignChart />
+
+            <div className="ml-auto mt-5 flex w-[90%] items-start justify-between gap-2 text-sm">
+              <div className="flex items-start gap-6">
+                <div className="flex items-start gap-2">
+                  <span className="mt-1 inline-block h-2 w-2 rounded-full bg-blue-400"></span>
+                  <div className="">
+                    <span className="text-sm text-[#828282]">
+                      Total campagn
+                    </span>
+                    <p className="font-semibold text-[#333333]">54</p>
+                  </div>
+                </div>
+                <div className="flex items-start gap-2">
+                  <span className="mt-1 inline-block h-2 w-2 rounded-full bg-main-100"></span>
+                  <div className="">
+                    <span className="text-sm text-[#828282]">
+                      Total response
+                    </span>
+                    <p className="font-semibold text-[#333333]">569</p>
+                  </div>
+                </div>
+              </div>
+              <div className="text-right">
+                <p className="text-sm text-[#828282]">Amount spent</p>
+                <h4 className="font-semibold text-[#333333]">$2500</h4>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="rounded-2xl bg-white p-[14px]"></div>
+        <div className="rounded-2xl bg-white p-[14px]">
+          <h3 className="mb-10 text-base font-medium">Campaign summary</h3>
+          <CampaignSummary />
+        </div>
       </div>
       {/* RECENT RESPONSES */}
 
@@ -419,160 +428,3 @@ const StatusPill: React.FC<StatusPillProps> = ({ status }) => {
     </span>
   );
 };
-
-export const description = "A stacked area chart";
-
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 105, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-];
-
-const chartConfig = {
-  desktop: {
-    label: "Desktop",
-    color: "hsl(var(--chart-1))",
-  },
-  mobile: {
-    label: "Mobile",
-    color: "hsl(var(--chart-2))",
-  },
-} satisfies ChartConfig;
-
-export function CampaignChart() {
-  const [timeRange, setTimeRange] = React.useState("90d");
-  const [date, setDate] = useState("");
-  const [year, setYear] = useState<number>(new Date().getFullYear());
-  const [month, setMonth] = useState<number>(new Date().getMonth());
-
-  // Generates an array of years for DOB selection (adjust range as needed)
-  const years = Array.from(
-    { length: 100 },
-    (_, i) => new Date().getFullYear() - i,
-  );
-
-  const filteredData = chartData.filter((item) => {
-    // const date = new Date(item.date);
-    const date = new Date();
-    const now = new Date();
-    let daysToSubtract = 90;
-    if (timeRange === "30d") {
-      daysToSubtract = 30;
-    } else if (timeRange === "7d") {
-      daysToSubtract = 7;
-    }
-    now.setDate(now.getDate() - daysToSubtract);
-    return date >= now;
-  });
-  return (
-    <>
-      <Card className="border-0">
-        <CardHeader className="flex items-center gap-2 space-y-0 border-b border-none py-5 sm:flex-row">
-          <div className="grid flex-1 gap-1 text-center sm:text-left">
-            <CardTitle className="text-base font-medium">
-              Campaign against response
-            </CardTitle>
-          </div>
-        </CardHeader>
-        <CardContent className="border-0 p-0">
-          <ChartContainer config={chartConfig} className="h-[300px]">
-            <AreaChart
-              accessibilityLayer
-              data={chartData}
-              margin={{
-                left: 12,
-                right: 12,
-              }}
-            >
-              <CartesianGrid vertical={false} />
-              <XAxis
-                dataKey="month"
-                tickLine={true}
-                axisLine={false}
-                tickMargin={8}
-                tickFormatter={(value) => value.slice(0, 3)}
-              />
-              {/* <YAxis
-                dataKey="month"
-                tickLine={true}
-                axisLine={false}
-                tickMargin={8}
-                tickFormatter={(value) => value.slice(0, 3)}
-              /> */}
-              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-              <defs>
-                <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="5%"
-                    stopColor="var(--color-desktop)"
-                    stopOpacity={0.8}
-                  />
-                  <stop
-                    offset="95%"
-                    stopColor="var(--color-desktop)"
-                    stopOpacity={0.1}
-                  />
-                </linearGradient>
-                <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                  <stop
-                    offset="5%"
-                    stopColor="var(--color-mobile)"
-                    stopOpacity={0.8}
-                  />
-                  <stop
-                    offset="95%"
-                    stopColor="var(--color-mobile)"
-                    stopOpacity={0.1}
-                  />
-                </linearGradient>
-              </defs>
-              <Area
-                dataKey="mobile"
-                type="natural"
-                fill="url(#fillMobile)"
-                fillOpacity={0.4}
-                stroke="var(--color-mobile)"
-                stackId="a"
-              />
-              <Area
-                dataKey="desktop"
-                type="natural"
-                fill="url(#fillDesktop)"
-                fillOpacity={0.4}
-                stroke="var(--color-desktop)"
-                stackId="a"
-              />
-            </AreaChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter>
-          <div className="flex w-full items-start justify-between gap-2 text-sm">
-            <div className="flex items-start gap-6">
-              <div className="flex items-start gap-2">
-                <span className="mt-1 inline-block h-2 w-2 rounded-full bg-blue-400"></span>
-                <div className="">
-                  <span className="text-sm text-[#828282]">Total campagn</span>
-                  <p className="font-semibold text-[#333333]">54</p>
-                </div>
-              </div>
-              <div className="flex items-start gap-2">
-                <span className="mt-1 inline-block h-2 w-2 rounded-full bg-main-100"></span>
-                <div className="">
-                  <span className="text-sm text-[#828282]">Total response</span>
-                  <p className="font-semibold text-[#333333]">569</p>
-                </div>
-              </div>
-            </div>
-            <div className="text-right">
-              <p className="text-sm text-[#828282]">Amount spent</p>
-              <h4 className="font-semibold text-[#333333]">$2500</h4>
-            </div>
-          </div>
-        </CardFooter>
-      </Card>
-    </>
-  );
-}
