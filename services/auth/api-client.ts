4fef8e38eea78b0b90650e9299c7993bf66a42e2
Williams <widorenyin0@gmail.com>
2025-02-13 10:24:32 +0100

    updates and fixes for build issues

--- services/auth/api-client.ts
+++ services/auth/api-client.ts
@@ -1,7 +1,7 @@
 
 import { baseURL } from "@/lib/axiosInstance";
 import axios from "axios";
-
+//remember to check and resolve some fixes
 const apiClient = axios.create({
   baseURL: baseURL,
   headers: {
